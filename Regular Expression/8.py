14d7aca3c9bb2d9398ec665f6bf4ce8356638e42
HimanshuHeda <harsh967896@gmail.com>
2024-08-12 01:23:05 +0530

    Regular Expression1

---
+++ Regular Expression/8.py
@@ -0,0 +1,6 @@
+# 1. Sum:
+
+import numpy as np # Import the NumPy library
+array = np.array([1,2,5,4]) # Create a NumPy array
+total = np.sum(array) # Compute the sum of the array elements
+print(total) # Print the sum
---
+++ Regular Expression/9.py
@@ -0,0 +1,6 @@
+# 2. Mean :
+
+import numpy as np # Import the NumPy library
+array = np.array([1,2,5,4]) # Create a NumPy array
+mean_value = np.mean(array)    # Compute the mean of the array elements
+print(mean_value)            # Print the mean
