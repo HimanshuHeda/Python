cb697535aa2757e55ca5fcf885e19cb26504dd17
HimanshuHeda <harsh967896@gmail.com>
2024-08-27 12:33:21 +0530

    matplotlib

---
+++ Assignment/matplotlib/histogram.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.normal(170,10,230)
+
+plt.hist(x)
+plt.title('Pie Chat')
+plt.show()
---
+++ Assignment/matplotlib/histogram1.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt 
+
+data = []
+
+plt.hist(x)
+plt.title('Histogram')
+plt.show()
