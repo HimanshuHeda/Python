213cfdc0ec6016d8be37aaf69c94e8d68798d8a1
HimanshuHeda <harsh967896@gmail.com>
2024-08-28 00:08:48 +0530

    matplotlib

---
+++ Assignment/matplotlib/area_chart.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 6, 8, 2]
+
+plt.fill_between(x, y)
+plt.title('Area Chart')
+plt.show()
+
+plt.fill_between([1, 2, 3, 4, 5], [1, 4, 6, 8, 2])
+plt.title('Area Chart')
+plt.show()
