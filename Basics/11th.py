b9b1987a90384c679c6df6c82d82373361feb8d1
HimanshuHeda <harsh967896@gmail.com>
2024-12-15 23:06:07 +0530

    factorials

---
+++ Basics/11th.py
@@ -0,0 +1,12 @@
+# Function to calculate factorial
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# Main Program
+number = int(input("Enter a number to find its factorial: "))
+print(f"The factorial of {number} is {factorial(number)}")
